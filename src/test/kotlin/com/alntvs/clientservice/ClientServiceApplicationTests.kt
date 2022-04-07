a2f22808972b28851da0f7e97f9682d6a538f3f5
AlnTVS <alntvs@yandex.ru>
2022-04-07 12:25:59 +0300

    Integration test for REST /update method.

--- src/test/kotlin/com/alntvs/clientservice/ClientServiceApplicationTests.kt
+++ src/test/kotlin/com/alntvs/clientservice/ClientServiceApplicationTests.kt
@@ -2,6 +2,8 @@ package com.alntvs.clientservice
 
 import com.alntvs.clientservice.entity.ClientEntity
 import com.alntvs.clientservice.model.ClientDTO
+import com.alntvs.clientservice.model.DataDto
+import com.alntvs.clientservice.model.ErrorDto
 import com.alntvs.clientservice.repository.ClientRepository
 import com.google.gson.GsonBuilder
 import org.assertj.core.api.Assertions.assertThat
@@ -22,20 +24,23 @@ class ClientServiceApplicationTests(
     val clientRepository: ClientRepository
 ) {
 
-    val gson = GsonBuilder().serializeNulls().create()
+    val gson = GsonBuilder().serializeNulls().disableHtmlEscaping().create()
 
     @Test
     fun `create REST by correct request`() {
         val clientDTO = ClientDTO(null, "username1", "Name", "Surname", "e@mail.com", "+007", "Russia, Moscow", true)
         val jsonClientDTO = gson.toJson(clientDTO)
+        val dataDto = DataDto("success")
+        val jsonDataDto = gson.toJson(dataDto)
 
         val httpHeaders = HttpHeaders()
         httpHeaders.set("Content-Type", "application/json")
         val httpEntity = HttpEntity(jsonClientDTO, httpHeaders)
 
-        val body = restTemplate.postForEntity("/user/create", httpEntity, String::class.java)
+        val response = restTemplate.postForEntity("/user/create", httpEntity, String::class.java)
 
-        assertThat(body.statusCodeValue).isEqualTo(200)
+        assertThat(response.statusCodeValue).isEqualTo(200)
+        assertThat(response.body).isEqualTo(jsonDataDto)
     }
 
     @Test
@@ -52,16 +57,15 @@ class ClientServiceApplicationTests(
         httpHeaders.set("Content-Type", "application/json")
         val httpEntity = HttpEntity(jsonClientEntity, httpHeaders)
 
-        val body = restTemplate.postForEntity("/user/create", httpEntity, String::class.java)
+        val response = restTemplate.postForEntity("/user/create", httpEntity, String::class.java)
 
-        assertThat(body.statusCodeValue).isEqualTo(200)
-        assertThat(body.body).contains(errorMsg)
+        assertThat(response.statusCodeValue).isEqualTo(200)
+        assertThat(response.body).contains(errorMsg)
     }
 
     @Test
     fun `create REST by incorrect request, username is null`() {
-        val clientDTO =
-            ClientDTO(null, null)
+        val clientDTO = ClientDTO(null, null)
         val errorMsg = "Field 'userName' must be filled!"
         val jsonClientEntity = gson.toJson(clientDTO)
 
@@ -69,10 +73,105 @@ class ClientServiceApplicationTests(
         httpHeaders.set("Content-Type", "application/json")
         val httpEntity = HttpEntity(jsonClientEntity, httpHeaders)
 
-        val body = restTemplate.postForEntity("/user/create", httpEntity, String::class.java)
+        val response = restTemplate.postForEntity("/user/create", httpEntity, String::class.java)
 
-        assertThat(body.statusCodeValue).isEqualTo(200)
-        assertThat(body.body).contains(errorMsg)
+        assertThat(response.statusCodeValue).isEqualTo(200)
+        assertThat(response.body).contains(errorMsg)
+    }
+
+    @Test
+    fun `update REST by correct request`() {
+        val clientDTO =
+            ClientDTO(1, "username1", "Name", "Surname", "e@mail.com", "+007", "Russia, Saint-Petersburg", true)
+        val jsonClientEntity = gson.toJson(clientDTO)
+        val expectedData = DataDto("success")
+        val expectedBody = gson.toJson(expectedData)
+        clientRepository.save(ClientEntity(userName = "username"))
+
+        val httpHeaders = HttpHeaders()
+        httpHeaders.set("Content-Type", "application/json")
+        val httpEntity = HttpEntity(jsonClientEntity, httpHeaders)
+
+        val response = restTemplate.postForEntity("/user/update", httpEntity, String::class.java)
+
+        assertThat(response.statusCodeValue).isEqualTo(200)
+        assertThat(response.body).isEqualTo(expectedBody)
+    }
+
+    @Test
+    fun `update REST by incorrect request, null id`() {
+        val clientDTO =
+            ClientDTO(null, "username1", "Name", "Surname", "e@mail.com", "+007", "Russia, Saint-Petersburg", true)
+        val jsonClientEntity = gson.toJson(clientDTO)
+        val expectedMsg = "Field 'id' and 'userName' must be filled!"
+
+        val httpHeaders = HttpHeaders()
+        httpHeaders.set("Content-Type", "application/json")
+        val httpEntity = HttpEntity(jsonClientEntity, httpHeaders)
+
+        val response = restTemplate.postForEntity("/user/update", httpEntity, String::class.java)
+
+        assertThat(response.statusCodeValue).isEqualTo(200)
+        assertThat(response.body).contains(expectedMsg)
+    }
+
+    @Test
+    fun `update REST by correct request, null username`() {
+        val clientDTO =
+            ClientDTO(1, null, "Name", "Surname", "e@mail.com", "+007", "Russia, Saint-Petersburg", true)
+        val jsonClientEntity = gson.toJson(clientDTO)
+        val expectedData = DataDto("success")
+        val expectedBody = gson.toJson(expectedData)
+        clientRepository.save(ClientEntity(userName = "username"))
+
+        val httpHeaders = HttpHeaders()
+        httpHeaders.set("Content-Type", "application/json")
+        val httpEntity = HttpEntity(jsonClientEntity, httpHeaders)
+
+        val response = restTemplate.postForEntity("/user/update", httpEntity, String::class.java)
+
+        assertThat(response.statusCodeValue).isEqualTo(200)
+        assertThat(response.body).isEqualTo(expectedBody)
+    }
+
+    @Test
+    fun `update REST by incorrect request, new already exist username`() {
+        val username = "username"
+        val clientEntity1 = ClientEntity(userName = username)
+        val clientEntity2 = ClientEntity(userName = "other username")
+        val clientDTO =
+            ClientDTO(2, username)
+        val jsonClientEntity = gson.toJson(clientDTO)
+        val expectedMsg = "Client with username:$username already exists!"
+        clientRepository.save(clientEntity1)
+        clientRepository.save(clientEntity2)
+
+        val httpHeaders = HttpHeaders()
+        httpHeaders.set("Content-Type", "application/json")
+        val httpEntity = HttpEntity(jsonClientEntity, httpHeaders)
+
+        val response = restTemplate.postForEntity("/user/update", httpEntity, String::class.java)
+
+        assertThat(response.statusCodeValue).isEqualTo(200)
+        assertThat(response.body).contains(expectedMsg)
+    }
+
+    @Test
+    fun `update REST by incorrect request, doesn't exist id`() {
+        val id: Long = 1
+        val clientDTO =
+            ClientDTO(id)
+        val jsonClientEntity = gson.toJson(clientDTO)
+        val expectedMsg = "Client with id:$id doesn't exists!"
+
+        val httpHeaders = HttpHeaders()
+        httpHeaders.set("Content-Type", "application/json")
+        val httpEntity = HttpEntity(jsonClientEntity, httpHeaders)
+
+        val response = restTemplate.postForEntity("/user/update", httpEntity, String::class.java)
+
+        assertThat(response.statusCodeValue).isEqualTo(200)
+        assertThat(response.body).contains(expectedMsg)
     }
 
 }
